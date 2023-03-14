4c58c557eb0ed807dac65aad43001322bc605b37
sucad hassan <sucad1@hotmail.co.uk>
2023-03-14 20:01:13 +0000

    added header and stles css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,17 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
     return <div>
-        Home
+        <header className="header">
+            <Link to ="/">Home</Link>
+            <Link to ="contact">Contct</Link>
+            <Link to ="project">Project</Link>
+        </header>
+        <section>
+
+
+        </section>
+
     </div>
 }
 
