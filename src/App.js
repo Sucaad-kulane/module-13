e07d44f65b9cf6a5741d91f08417aa4cdca7a0c7
sucad hassan <sucad1@hotmail.co.uk>
2023-03-14 19:40:51 +0000

    testing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Home from './pages/Home'
 function App() {
   return (
-    <div >
-      <header>
+    <Router>
+      <div >
+        <Routes>
+          <Route path="/" element={Home}>
 
-      </header>
+          </Route>
 
-      <footer>
-        
-      </footer>
-      
-    </div>
+        </Routes>
+
+
+      </div>
+    </Router>
   );
 }
 
