2c34be9a345b8d03fc25fc10b976fd0ce817bd35
sucad hassan <sucad1@hotmail.co.uk>
2023-03-14 19:50:18 +0000

    added pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Home from './pages/Home'
+import Contact from './pages/Contact';
+import Project from './pages/Project';
+
 function App() {
   return (
     <Router>
       <div >
+        
         <Routes>
-          <Route path="/" element={Home}>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+          <Route path="/project" element={<Project/>}/>
 
-          </Route>
+
 
         </Routes>
 
