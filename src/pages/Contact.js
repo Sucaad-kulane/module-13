7e94250a1b91faffbb624f0f87db6a567b80ef5f
sucad hassan <sucad1@hotmail.co.uk>
2023-03-15 21:41:11 +0000

    Updated application

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,27 +1,23 @@
 import { FaLinkedin, FaTwitterSquare, FaGithub } from 'react-icons/fa';
 
-const Contact = ({name, title}) => {
+const Contact = ({ name, title }) => {
     return <div>
         <section className="front">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeGHwmhCFbrbiSUkl-tz4SeNDIjSYU-Bp0Aw&usqp=CAU"/>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeGHwmhCFbrbiSUkl-tz4SeNDIjSYU-Bp0Aw&usqp=CAU" />
         </section>
-        <h1>Find me here: </h1>
-        <li className="list-inline-item mr-3">
-              <a href="#">
-                <FaLinkedin className="fa-3x fa-fw" />
-              </a> 
-            </li>
-            <li className="list-inline-item mr-3">
-              <a href="#">
-                <FaTwitterSquare className="fa-3x fa-fw" />
-              </a> 
-            </li>
-            <li className="list-inline-item mr-3">
-              <a href="#">
-                <FaGithub className="fa-3x fa-fw" />
-              </a> 
-            </li>
-        
+        <div style={{ textAlign: 'center' }}>
+            <h1>Find me here:</h1>
+        </div>
+
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <a href="https://www.linkedin.com/in/sucaad-kulane/" style={{ marginRight: '1em' }}>
+                <FaLinkedin className="fa-5x fa-fw" style={{ fontSize: '5em' }} />
+            </a>
+            <a href="https://github.com/Sucaad-kulane">
+                <FaGithub className="fa-5x fa-fw" style={{ fontSize: '5em' }} />
+            </a>
+        </div>
+
     </div>
 }
 export default Contact;
